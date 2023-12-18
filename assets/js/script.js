a1a3259bf5324f7c57e338b9e01751618b8815f5
Jonas Hessling <firstname.jonas@icloud.com>
2023-12-18 10:24:47 +0000

    Add variables with html elements id i want to target

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// HTML elements I want to target
+const questionElement = document.getElementById('question');
+const yesButton = document.getElementById('answer-yes');
+const noButton = document.getElementById('answer-no');
