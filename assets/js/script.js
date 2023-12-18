7e49b9887054d5e445868dfadaa6193bd3820cb5
Jonas Hessling <firstname.jonas@icloud.com>
2023-12-18 10:58:47 +0000

    Add an array with questions and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,42 @@
 const questionElement = document.getElementById('question');
 const yesButton = document.getElementById('answer-yes');
 const noButton = document.getElementById('answer-no');
+
+// Data for questions and answers that are displaying
+const decisionTree = [{
+    question: 'I would typically describe myself as:',
+    options: ['Creative', 'Analytical']
+},
+{
+    question: 'I am interested in technology:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I enjoy building things from scratch:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'A competitive salary is important to me:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I struggle with time management:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I enjoy problem-solving and completing puzzles:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I am self-motivated and can work alone:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I enjoy coming up with new ideas:',
+    options: ['Yes', 'No']
+},
+{
+    question: 'I struggle with thinking outside of the box:',
+    options: ['Yes', 'No']
+}
+]
